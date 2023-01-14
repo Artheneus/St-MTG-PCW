941b32cb413428037aa2f1d48a69be2a5f81082a
Wisnu Agung <65849194+Artheneus@users.noreply.github.com>
2023-01-14 09:21:23 +0800

    Delete HorRadButton directory

--- HorRadButton/pages/3_Card List.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-with open('style/style.css') as f :
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-st.title("This is the List where you added to the database")
