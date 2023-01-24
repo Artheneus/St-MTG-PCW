270ea44575a6e519160ed7cd45335c3a56a70bd3
Wisnu Agung <65849194+Artheneus@users.noreply.github.com>
2023-01-24 17:39:10 +0800

    Delete multipage directory

--- multipage/apps/load_local_css.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-class load_css:
-    def local_load(file_name):
-        with open(file_name) as f:
-            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
